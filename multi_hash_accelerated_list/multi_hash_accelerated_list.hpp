aa0c6611ddfa55a036a6886c2278f339b6713031
Vlad K <vkvitnevski@gmail.com>
2013-04-12 10:17:00 -0400

    Moved main to test, added new project for multi-item data structure

---
+++ multi_hash_accelerated_list/multi_hash_accelerated_list.hpp
@@ -0,0 +1,10 @@
+#include "hash_accelerated_list.hpp"
+
+using namespace std;
+
+template <typename T>
+class MultiHashAccelList
+{
+public:
+	MultiHashAccelList() {}
+};
---
+++ multi_hash_accelerated_list/test.cpp
@@ -0,0 +1,13 @@
+// multi_hash_accelerated_list.cpp : Defines the entry point for the console application.
+//
+
+#include "multi_hash_accelerated_list.hpp"
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
