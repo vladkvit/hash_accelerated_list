fddd734f8d519f3bf9a221dbe1106fc2224a4485
Vlad K <vkvitnevski@gmail.com>
2013-06-21 19:58:59 -0400

    Formattinig changes, boost ifdefs.

--- hash_accelerated_list/hash_accelerated_list.hpp
+++ hash_accelerated_list/hash_accelerated_list.hpp
@@ -8,8 +8,6 @@
 
 #define USE_BOOST
 #ifdef USE_BOOST
-//#include <boost/config.hpp>
-//#include <boost/iterator.hpp>
 #include <boost/iterator/iterator_adaptor.hpp>
 #endif
 
@@ -28,16 +26,12 @@ public:
 
 	void remove( const T& ); //O(1)
 
-	//typename list< T >::const_iterator begin() const; //O(1)
-	//typename list< T >::const_iterator end() const; //O(1)
-	//typename list< T >::iterator begin(); //O(1)
-	//typename list< T >::iterator end(); //O(1)
-
 private:
 	list< T > ordered_list;
 	unordered_map< T, typename list< T >::iterator > indexing;
 
 public:
+#ifdef USE_BOOST
 	template <typename T, bool c> //c determines if it is a const_iterator or not
 	class _iterator : public
 		boost::iterator_adaptor<
@@ -99,6 +93,7 @@ public:
 	{
 		return h_const_iterator( ordered_list.end() );
 	}
+#endif
 };
 
 //Since I'm using templates, the implementation has to go into the header
@@ -132,11 +127,14 @@ void HashAccelList< T >::insert( typename list< T >::iterator new_item_it, const
 	}
 }
 
+#ifdef USE_BOOST
 template <typename T>
-void HashAccelList< T >::insert( typename HashAccelList< T >::h_iterator it, const T& val )
+void HashAccelList< T >::insert( typename HashAccelList< T >::h_iterator position, const T& val )
 {
-
+	list< T >::iterator pos = position.base_reference();
+	insert( pos, val );
 }
+#endif
 
 template <typename T>
 void HashAccelList< T >::insert_begin( const T& val )
@@ -151,82 +149,3 @@ void HashAccelList< T >::insert_end( const T& val )
 	list< T >::iterator new_item_it = ordered_list.end();
 	insert( new_item_it, val );
 }
-
-
-/*
-#include <boost/iterator/iterator_adaptor.hpp>
-#include <list>
-
-using namespace std;
-
-//forward declaration
-
-template <typename T>
-class HashAccelList
-{
-public:
-	//static const int ARR_SZ = 10;
-	//T myarr[ARR_SZ];
-	list<T> myl;
-
-public:
-	template <typename T, bool c> //c determines if it is a const_iterator or not
-	class _iterator : public
-		boost::iterator_adaptor<
-		_iterator<T, c>,
-		typename std::conditional< c, typename list<T>::const_iterator, typename list<T>::iterator >::type,
-		boost::use_default,
-		boost::bidirectional_traversal_tag >
-	{
-	private:
-		typedef boost::iterator_adaptor<
-			_iterator<T, c>,
-			typename std::conditional< c, typename list<T>::const_iterator, typename list<T>::iterator >::type,
-			boost::use_default,
-			boost::bidirectional_traversal_tag
-		> super_t;
-
-	public:
-		explicit _iterator( typename std::conditional<c, typename list<T>::const_iterator, typename list<T>::iterator>::type p )
-		: super_t(p) {}
-
-		//copy constructor so we can auto-convert iterators into const_iterators
-		//needed since begin() from non-const "this" only returns a non-const iterator
-		_iterator( _iterator<T, false>& it ) : super_t( it.base_reference() ) {}
-
-		void increment() { this->base_reference()++; }
-		void decrement() { this->base_reference()--; }
-
-		const T& operator*() const
-		{
-			list<T>::const_iterator it = this->base_reference();
-			return *(it);
-		}
-
-		friend class _iterator<T, true>;
-	};
-
-	typedef _iterator<T, false> h_iterator;
-	typedef _iterator<T, true> h_const_iterator;
-
-	h_iterator begin()
-	{
-		return h_iterator( myl.begin() );
-	}
-
-	h_iterator end()
-	{
-		return h_iterator( myl.end() );
-	}
-
-	h_const_iterator begin() const
-	{
-		return h_const_iterator( myl.begin() );
-	}
-
-	h_const_iterator end() const
-	{
-		return h_const_iterator( myl.end() );
-	}
-};
-*/
